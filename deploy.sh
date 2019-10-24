0c46e586bb17eafa31fa5dd496784127b29e4b7d
fedevors <fedevors@gmail.com>
2019-10-24 22:51:38 +0800

    travis test2

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:hefeng1208/Heartbreak-utopia.git master:gh-pages
+# git push -f git@github.com:hefeng1208/Heartbreak-utopia.git master:gh-pages
 
 # 如果使用 travis 持续集成
 git push -f https://${GITHUB_TOKEN}@github.com:hefeng1208/Heartbreak-utopia.git master:gh-pages
