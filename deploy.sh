4df8e91a14e08a67e742521281542a43e5fbd2bb
hefeng26 <hefeng3@jd.com>
2019-10-25 14:08:12 +0800

    init one more tryaa

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,19 @@
 set -e
 
 # 生成静态文件
-npm run build
+yarn build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
+git config --global user.email $GH_EMAIL
+git config --global user.name $GH_NAME
+
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m 'chore(deploy): deploy gh-pages'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:hefeng1208/Heartbreak-utopia.git master:gh-pages
+git push -f git@github.com:hefeng1208/circle-ci-test.git master:gh-pages
 
 cd -
